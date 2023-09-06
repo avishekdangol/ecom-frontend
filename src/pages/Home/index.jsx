af18d1acad5dd9bd600964880e7795d4d9f347b2
Avishek Dangol <dangolavishek@gmail.com>
2023-09-06 13:54:11 +0545

    axios setup

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,11 +1,7 @@
 import { Card } from "antd";
 import AppLayout from "../../layouts/layout";
-import useJwt from "../../useJWT/useJwt";
 
 const Home = () => {
-  useJwt.test().then(res => {
-    console.log(res)
-  })
   return (
     <>
       <AppLayout>
