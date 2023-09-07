0e61c53b0d8d75da50e6a6b100b731f8461f3a34
Avishek Dangol <dangolavishek@gmail.com>
2023-09-07 18:45:19 +0545

    icon added

--- src/components/navbar/Index.jsx
+++ src/components/navbar/Index.jsx
@@ -1,10 +1,21 @@
 import "@/scss/navbar.scss"
+import { Menu } from "antd";
+import navMenu from "@/navigation/navMenu";
 
 const Navbar = () => {
   return (
     <>
-      <h1>This is Navbar</h1>
-      <img className="logo" src="assets/images/logo.png" alt="" />
+      {/* <h1>This is Navbar</h1>
+      <img className="logo" src="assets/images/logo.png" alt="" /> */}
+       <Menu
+          mode="horizontal"
+          defaultSelectedKeys={['2']}
+          style={{paddingInline: 0}}
+          items={new Array(3).fill(null).map((_, index) => ({
+            key: String(index + 1),
+            label: `nav ${index + 1}`,
+          }))}
+        />
     </>
   );
 };
