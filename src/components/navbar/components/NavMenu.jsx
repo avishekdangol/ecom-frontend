98a2ec1690e1c5ed34ea4ee82af56ea92e685138
pawan <pulamigaurav8@gmail.com>
2023-10-14 19:54:19 +0545

    mega menu added

--- src/components/navbar/components/NavMenu.jsx
+++ src/components/navbar/components/NavMenu.jsx
@@ -1,46 +1,89 @@
-import { Button, Dropdown , Row , Col , Space} from "antd";
+import { useState } from "react";
+import { Button, Row, Col } from "antd";
 import { HiMenuAlt4 } from "react-icons/hi";
 
 function NavMenu() {
-  const items = [
-    {
-      key: "1",
-      label: (
-        <Row>
-           <Col span={12} className="w-96 h-96">
-             <div className="p-5">
-               <ul>
-                 <h4>title name</h4>
-                 <li><a href="/">nav menu</a></li>
-                 <li>nav menu</li>
-                 <li>nav menu</li>
-                 <li>nav menu</li>
-               </ul>
-             </div>
-           </Col>
-          <Col span={12}>
-          <div className="p-5">
-               <ul>
-                 <h4>title name</h4>
-                 <li><a href="/">nav menu</a></li>
-                 <li>nav menu</li>
-                 <li>nav menu</li>
-                 <li>nav menu</li>
-               </ul>
-             </div>
-          </Col>
-        </Row>
-      ),
-    },
-  ];
-
+  const [toggle, setToggle] = useState(true);
+  const toggleFunction = () => {
+    setToggle(!toggle);
+  };
   return (
-    <Dropdown menu={{ items }} placement="bottomLeft" trigger="click" arrow>
-      <Button className="flex items-center justify-center" shape="round">
+    <section className="dropdown">
+      <Button
+        className="flex items-center justify-center"
+        shape="round"
+        onClick={toggleFunction}
+      >
         <HiMenuAlt4 className="me-1" />
         Menu
       </Button>
-    </Dropdown>
+      <div
+        className={`absolute z-50 bg-white dropdown-content rounded-xl p-5 ${
+          toggle ? "closed" : "open"
+        } `}
+      >
+        <Row gutter={[0, 2]}>
+          <Col md={6} sm={8}>
+            <ul>
+              <h5>Men</h5>
+              <li>
+                <a href="#">Casual</a>
+              </li>
+              <li>
+                <a href="#">90s</a>
+              </li>
+              <li>
+                <a href="#">Boots</a>
+              </li>
+              <li>
+                <a href="#">Loafer</a>
+              </li>
+            </ul>
+          </Col>
+          <Col md={6} sm={8}>
+            <ul>
+              <h4>Women</h4>
+              <li>
+                <a href="#">Flats</a>
+              </li>
+              <li>
+                <a href="#">Heels</a>
+              </li>
+              <li>
+                <a href="#">Mules</a>
+              </li>
+              <li>
+                <a href="#">Shippers</a>
+              </li>
+            </ul>
+          </Col>
+          <Col md={6} sm={8}>
+            <ul>
+              <h4>Unisex</h4>
+              <li>
+                <a href="#">Sneaker</a>
+              </li>
+              <li>
+                <a href="#">Scandals</a>
+              </li>
+              <li>
+                <a href="#">Hiking</a>
+              </li>
+              <li>
+                <a href="#">Sports</a>
+              </li>
+            </ul>
+          </Col>
+          <Col md={6} sm={24}>
+            <img
+              src="/assets/menu/shoes.jpg"
+              alt=""
+              className="image-objects-fit"
+            />
+          </Col>
+        </Row>
+      </div>
+    </section>
   );
 }
 
--- src/pages/Home/Index.jsx
+++ src/pages/Home/Index.jsx
@@ -1,21 +1,21 @@
-import { Card } from 'antd';
-import AppLayout from '@/layouts/Layout';
-import ItemsRow from '@/components/reusables/ItemsRow';
-import Jumbotron from '@/components/reusables/Jumbotron';
-import Carousels from '../../components/carousel';
+// import { Card } from 'antd';
+import AppLayout from "@/layouts/Layout";
+import ItemsRow from "@/components/reusables/ItemsRow";
+import Jumbotron from "@/components/reusables/Jumbotron";
+import Carousels from "../../components/carousel";
 
 function Home() {
   const items = new Array(4).fill().map((arr, index) => ({
     id: index,
-    title: 'Nike Air',
+    title: "Nike Air",
     price: 180.0,
     previousPrice: 249.0,
     colors:
       index % 3
-        ? ['bg-[#fff]', 'bg-[#df6e44]', 'bg-[#000]']
-        : ['bg-[#000]', 'bg-[#ae64f0]'],
+        ? ["bg-[#fff]", "bg-[#df6e44]", "bg-[#000]"]
+        : ["bg-[#000]", "bg-[#ae64f0]"],
     image:
-      'https://static.nike.com/a/images/t_PDP_1280_v1/f_auto,q_auto:eco/e9d41cd4-a2c5-4ca7-a3aa-f4bf597658d0/custom-nike-air-force-1-mid-by-you-shoes.png',
+      "https://static.nike.com/a/images/t_PDP_1280_v1/f_auto,q_auto:eco/e9d41cd4-a2c5-4ca7-a3aa-f4bf597658d0/custom-nike-air-force-1-mid-by-you-shoes.png",
     isNew: !!(index % 2),
     isHot: !!(index % 3),
   }));
