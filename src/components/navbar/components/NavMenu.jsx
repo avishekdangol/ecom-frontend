20da1c4df1f6d9256c78528b7fb0791fdd62f840
Avishek Dangol <dangolavishek@gmail.com>
2023-10-16 12:35:24 +0545

    refactor

--- src/components/navbar/components/NavMenu.jsx
+++ src/components/navbar/components/NavMenu.jsx
@@ -1,72 +1,54 @@
-import { useState } from "react";
-import { Link } from "react-router-dom";
-import { Button, Row, Col } from "antd";
-import { HiMenuAlt4 } from "react-icons/hi";
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { Button, Row, Col } from 'antd';
+import { HiMenuAlt4 } from 'react-icons/hi';
 
 /**
  * @static menu item for mega menu
  */
 const data = {
-  men: ["Casual", "90s", "Boots", "Loafers"],
-  women: ["Flats", "Heels", "Mules", "Slippers"],
-  unisex: ["Sneaker", "Scandals", "Hikings", "Sports"],
+  categories: [
+    { id: 1, title: 'Men', subCategories: ['Casual', '90s', 'Boots', 'Loafers'] },
+    { id: 2, title: 'Women', subCategories: ['Flats', 'Heels', 'Mules', 'Slippers'] },
+    { id: 3, title: 'Unisex', subCategories: ['Sneaker', 'Scandals', 'Hikings', 'Sports'] },
+  ],
   image: {
-    url: "#",
-    img: "assets/menu/shoes.jpg",
+    url: '#',
+    img: 'assets/menu/shoes.jpg',
   },
 };
 
 function NavMenu() {
-  const [toggle, setToggle] = useState(true);
-  const toggleFunction = () => {
-    setToggle(!toggle);
+  const [showMenu, setShowMenu] = useState(false);
+  const toggleMenu = () => {
+    setShowMenu(!showMenu);
   };
   return (
     <section className="dropdown">
       <Button
         className="flex items-center justify-center"
         shape="round"
-        onClick={toggleFunction}
+        onClick={toggleMenu}
       >
         <HiMenuAlt4 className="me-1" />
         Menu
       </Button>
-      <div
-        className={`absolute z-50 bg-white dropdown-content rounded-xl p-5 ${
-          toggle ? "closed" : "open"
-        } `}
-      >
+      <div className={`nav-menu absolute z-50 bg-white dropdown-content rounded-xl p-5 ${showMenu && 'show'} `}>
         <Row gutter={[0, 2]}>
-          <Col md={6} sm={8}>
-            <ul className="p-0">
-              <h5>Men</h5>
-              {data.men.map((li) => (
-                <li key={li}>
-                  <Link to="#">{li}</Link>
-                </li>
-              ))}
-            </ul>
-          </Col>
-          <Col md={6} sm={8}>
-            <ul>
-              <h4>Women</h4>
-              {data.women.map((li) => (
-                <li key={li}>
-                  <Link to="#">{li}</Link>
-                </li>
-              ))}
-            </ul>
-          </Col>
-          <Col md={6} sm={8}>
-            <ul>
-              <h4>Unisex</h4>
-              {data.unisex.map((li) => (
-                <li key={li}>
-                  <Link to="#">{li}</Link>
-                </li>
-              ))}
-            </ul>
-          </Col>
+          {
+            data.categories.map((category) => (
+              <Col key={category.id} md={6} sm={8}>
+                <h5>{category.title}</h5>
+                <ul className="p-0">
+                  {category.subCategories.map((subCategory) => (
+                    <li key={subCategory}>
+                      <Link to="#">{subCategory}</Link>
+                    </li>
+                  ))}
+                </ul>
+              </Col>
+            ))
+          }
           <Col md={6} sm={24}>
             <Link to={data.image.url}>
               <img src={data.image.img} alt="" className="image-objects-fit" />
